7c191fc1e7013776e33b765c1ef2f846f6b7a4a3
arm <a01736035@tec.mx>
2022-11-17 23:30:50 -0600

    update final a python

--- python/view_readings.py
+++ python/view_readings.py
@@ -17,10 +17,15 @@ db = firebase.database() #se accesa a la base de datos de fyrebase
 info = db.child("readings").get() #entrar a la ruta específica donde están los elementos
 print("Inicializando lectura de base de datos")
 print()
+flag=False; #este bool se usa para detectar el cambio en usuario e imprimir un aviso del mismo
 for item in info.each(): #imprime para cada elemento en readings/
+    if(item.key()=="usuario"):
+        print(f' Elemento {item.key()} = {item.val()}')
     if(item.key()=="numero"): #caso especifico para el dato _numero_
         if(item.val()>9):
-            print(" Numero mayor que 9")
+            db.child("readings").update({"usuario":"elemento mayor a 9"}) #hace el cambio en campo usuario
+            flag=True;
+            print(" Numero mayor que 9... ")
         else:
             print(f' Numero = {item.val()}')    
     elif("temperaturaC" == item.key()): #caso para temperatura
@@ -29,17 +34,19 @@ for item in info.each(): #imprime para cada elemento en readings/
         print(f' Elemento {item.key()} = {item.val()}°F')
     elif("humedad" == item.key()): #caso para humedad 
         print(f' Elemento {item.key()} = {item.val()}%')
-    elif("distancia" == item.key()): #caso para distancia
+    elif("distance" == item.key()): #caso para distancia
         print(f' Elemento {item.key()} = {item.val()} cm.')
     elif("isFlame" == item.key()): #caso para el booleano que indica si hay flama
         if(item.val()==False):
             print(" No hay presencia de flama")
         else:
             print(" Hay presencia de flama")
-    elif("movimiento"==item.key()): #caso para booleano de movimiento
+    elif("movement"==item.key()): #caso para booleano de movimiento
         if(item.val()==False):
             print(" No hay presencia de movimiento")
         else:
             print(" Hay presencia de movimiento")
 print()
+if(flag==True):
+    print("Campo de usuario se actualizará a 'elemento mayor a 9'") #en proxima impresión el cambio se reflejará, en esta no porque toda la información se descargó antes de la modificación
 print("Lectura finalizada. Gracias!")
