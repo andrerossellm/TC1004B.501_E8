18ec7c2d2ffe57be28cac1b8e852b8fa0d1c47a8
Felipe <a01735939@tec.mx>
2022-11-18 00:10:50 -0600

    Changes in prubea_display

--- arduino/prueba_display.ino
+++ arduino/prueba_display.ino
@@ -1,9 +1,5 @@
-//se demuestra el manejo del display de siete segmentos
-
 //se declaran los pines a usar para el display
-//int LEDs[] = {8,9,7,6,4,3,2};        // for Arduino microcontroller
-//int LEDs[] = {D2,D1,D3,D4,D6,D7,D8}; // for ESP8266 microcontroller
-int LEDs[] = {25,16,5,18,21,3,1};    // for ESP32 microcontroller
+int LEDs[] = {25,16,5,18,21,3,1};
 //25 g, 16 f, 5 e, 18 d, 3 c, 21 b, 1 a    
 
 //se declaran los arreglos que forman los dígitos
@@ -24,65 +20,46 @@ int thirteen[] = {1, 0, 1, 1, 1, 1, 0};   // trece, d
 int fourteen[] = {1, 1, 1, 1, 0, 0, 1};   // catorce, E
 int fifteen[] = {1, 1, 1, 0, 0, 0, 1};   // quince, F
 
-//se declara contador
+//se variable declara contador
 unsigned char contador = 0;
 
 //función que despliega del 0 al F
-void segment_display(unsigned char valor)
-{
-    switch(valor)
-    {
+void segment_display(unsigned char valor){
+    switch(valor){
         case 0:
-                    for (int i = 0; i<7; i++) digitalWrite(LEDs[i], zero[i]);
-                    break;
+            for (int i = 0; i<7; i++) digitalWrite(LEDs[i], zero[i]); break;
         case 1:
-                    for (int i = 0; i<7; i++) digitalWrite(LEDs[i], one[i]);
-                    break;
+            for (int i = 0; i<7; i++) digitalWrite(LEDs[i], one[i]); break;
         case 2:
-                    for (int i = 0; i<7; i++) digitalWrite(LEDs[i], two[i]);
-                    break;
+            for (int i = 0; i<7; i++) digitalWrite(LEDs[i], two[i]); break;
         case 3:
-                    for (int i = 0; i<7; i++) digitalWrite(LEDs[i], three[i]);
-                    break;
+            for (int i = 0; i<7; i++) digitalWrite(LEDs[i], three[i]); break;
         case 4:
-                    for (int i = 0; i<7; i++) digitalWrite(LEDs[i], four[i]);
-                    break;
+            for (int i = 0; i<7; i++) digitalWrite(LEDs[i], four[i]); break;
         case 5:
-                    for (int i = 0; i<7; i++) digitalWrite(LEDs[i], five[i]);
-                    break;
+            for (int i = 0; i<7; i++) digitalWrite(LEDs[i], five[i]); break;
         case 6:
-                    for (int i = 0; i<7; i++) digitalWrite(LEDs[i], six[i]);
-                    break;
+            for (int i = 0; i<7; i++) digitalWrite(LEDs[i], six[i]); break;
         case 7:
-                    for (int i = 0; i<7; i++) digitalWrite(LEDs[i], seven[i]);
-                    break;
+            for (int i = 0; i<7; i++) digitalWrite(LEDs[i], seven[i]); break;
         case 8:
-                    for (int i = 0; i<7; i++) digitalWrite(LEDs[i], eight[i]);
-                    break;
+            for (int i = 0; i<7; i++) digitalWrite(LEDs[i], eight[i]); break;
         case 9:
-                    for (int i = 0; i<7; i++) digitalWrite(LEDs[i], nine[i]);
-                    break;
+            for (int i = 0; i<7; i++) digitalWrite(LEDs[i], nine[i]); break;
         case 10:
-                    for (int i = 0; i<7; i++) digitalWrite(LEDs[i], ten[i]);
-                    break;
+            for (int i = 0; i<7; i++) digitalWrite(LEDs[i], ten[i]); break;
         case 11:
-                    for (int i = 0; i<7; i++) digitalWrite(LEDs[i], eleven[i]);
-                    break;
+            for (int i = 0; i<7; i++) digitalWrite(LEDs[i], eleven[i]); break;
         case 12:
-                    for (int i = 0; i<7; i++) digitalWrite(LEDs[i], twelve[i]);
-                    break;
+            for (int i = 0; i<7; i++) digitalWrite(LEDs[i], twelve[i]); break;
         case 13:
-                    for (int i = 0; i<7; i++) digitalWrite(LEDs[i], thirteen[i]);
-                    break;
+            for (int i = 0; i<7; i++) digitalWrite(LEDs[i], thirteen[i]); break;
         case 14:
-                    for (int i = 0; i<7; i++) digitalWrite(LEDs[i], fourteen[i]);
-                    break;
+            for (int i = 0; i<7; i++) digitalWrite(LEDs[i], fourteen[i]); break;
         case 15:
-                    for (int i = 0; i<7; i++) digitalWrite(LEDs[i], fifteen[i]);
-                    break; 
+            for (int i = 0; i<7; i++) digitalWrite(LEDs[i], fifteen[i]); break; 
         default:
-                    for (int i = 0; i<7; i++) digitalWrite(LEDs[i], zero[i]);
-                    break;          
+            for (int i = 0; i<7; i++) digitalWrite(LEDs[i], zero[i]); break;          
     }
 }
 
